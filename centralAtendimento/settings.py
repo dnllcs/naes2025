091fb17d81c85afc2b629094a479173befcddc5f
dnllcs <daniellcs04@gmail.com>
2025-08-07 20:02:43 -0300

    fix: atualizado para uso de sqlite

--- centralAtendimento/settings.py
+++ centralAtendimento/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = "centralAtendimento.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres.tuiwhmkdgqsqiqxuvbnz",
-        "PASSWORD": "",
-        "HOST": "aws-0-sa-east-1.pooler.supabase.com",
-        "PORT": "6543",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres.tuiwhmkdgqsqiqxuvbnz",
+#         "PASSWORD": "",
+#         "HOST": "aws-0-sa-east-1.pooler.supabase.com",
+#         "PORT": "6543",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = 'static_gcloud/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
